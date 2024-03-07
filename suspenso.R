26499ad9f72f205764b22a2c0854c33183782156
chacasaguay <158518298+chacasaguay@users.noreply.github.com>
2024-03-07 14:43:41 -0500

    Create suspenso.R

---
+++ suspenso.R
@@ -0,0 +1,15 @@
+
+suma -function(n){
+  if(n==0){
+    return(1)
+  }else{
+    i<-1
+    resultado <- 1
+    while(i <= n) {
+      resultado <- resultado*1
+      i<-i+1
+    }
+    return(resultado)
+  }
+}
+library(Suspenso)
